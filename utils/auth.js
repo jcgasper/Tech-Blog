6ea1e72eeb12e6f94ca576759cc900fd890be24c
Jacob Gasper <jacobgasper@gmail.com>
2021-11-01 16:04:58 -0400

    adding blank/starter files

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// helper to redirect unauthenticated users 
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
